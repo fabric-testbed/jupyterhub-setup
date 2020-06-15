d768ca5e1c59fca2695bdf0a3391fbc664b15fe6
Erica Fu <ericafu@renci.org>
2020-06-15 16:05:46 -0400

    update template address for gitpuller

--- software/jupyterhub-docker/notebook/docker-entrypoint.sh
+++ software/jupyterhub-docker/notebook/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 # If the run command is the default, do some initialization first
 if [ "$(which "$1")" = "/usr/local/bin/start-singleuser.sh" ]; then
-  gitpuller https://github.com/ericafu1122/template master template
+  gitpuller https://github.com/fabric-testbed/jupyternb-template master template
 fi
 
 # Run the command provided
